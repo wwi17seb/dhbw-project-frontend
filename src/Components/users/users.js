0b6849687749d1cedaffffb1078f97875b281ef4
Erik Jansky <erik.jansky@gmail.com>
2019-12-02 14:21:51 +0100

    Added React Router and Sample Route
    Added React Router and Sample Route

---
+++ src/Components/users/users.js
@@ -0,0 +1,15 @@
+import React from 'react'
+class Users extends React.Component {
+  render() {
+    return (
+      <div className="User">
+      <header className="User-header">
+      <h1>
+        Users
+      </h1>
+      </header>
+    </div>
+      );
+  }
+}
+export default Users
