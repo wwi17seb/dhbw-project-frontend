cdb88878d2b602b619eddbdcbef0a789002665c2
David Letkemann <dsignupl@icloud.com>
2019-12-12 23:44:28 +0100

    Different table for 'Dozenten'-View

--- src/Components/dozenten/Dozenten.js
+++ src/Components/dozenten/Dozenten.js
@@ -1,67 +1,96 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import mail from '../../mail.svg';
 import remove from '../../delete.svg';
 import edit from '../../edit.svg';
 import './dozenten.css';
+import MaterialTable from 'material-table';
 import { enumDeclaration } from '@babel/types';
 
-class Dozenten extends Component {
-    render() {
-        return (
-            <table>
-                <tr className="tableHeaderRow">
-                    <td>Dozent/in</td>
-                    <td>Schwerpunkt</td>
-                    <td>Aktion</td>
-                </tr>
-                <tr>
-                    <td>
-                        Max Mustermann <br/>
-                        Tel: 0176 53725528 <br/>
-                        max.mustermann@dhbw-mannnheim.de
-                    </td>
-                    <td>
-                        Software-Architektur
-                    </td>
-                    <td>
-                        <img src={mail} alt="mail" height="25px" />
-                        <img src={edit} alt="edit" height="25px" />
-                        <img src={remove} alt="remove" height="25px" />
-                    </td>
-                </tr>
-                <tr>
-                <td>
-                        Max Mustermann <br/>
-                        Tel: 0176 53725528 <br/>
-                        max.mustermann@dhbw-mannnheim.de
-                    </td>
-                    <td>
-                        IT-Sicherheit
-                    </td>
-                    <td>
-                        <img src={mail} alt="mail" height="25px" />
-                        <img src={edit} alt="edit" height="25px" />
-                        <img src={remove} alt="remove" height="25px" />
-                    </td>
-                </tr>
-                <tr>
-                <td>
-                        Max Mustermann <br/>
-                        Tel: 0176 53725528 <br/>
-                        max.mustermann@dhbw-mannnheim.de
-                    </td>
-                    <td>
-                        Plattformen & Frameworks
-                    </td>
-                    <td>
-                        <img src={mail} alt="mail" height="25px" />
-                        <img src={edit} alt="edit" height="25px" />
-                        <img src={remove} alt="remove" height="25px" />
-                    </td>
-                </tr>
-            </table>
-        )
-    }
-}
 
-export default Dozenten
+export default function MaterialTableDemo() {
+    const [state, setState] = React.useState({
+        columns: [
+            { title: 'Vorname', field: 'vorname' },
+            { title: 'Nachname', field: 'nachname' },
+            { title: 'Schwerpunkt', field: 'schwerpunkt' },
+            { title: 'Kontaktdaten', field: 'mail' },
+            {
+                title: 'Dozentennummer',
+                field: 'id',
+                lookup: { 1: '12345', 2: '67890', 3: '13579' },
+            },
+        ],
+        data: [
+            { 
+                vorname: 'Michael',
+                nachname: 'Binzen',
+                schwerpunkt: 'Software-Architektur',
+                phone: '017653725528',
+                mail: 'michael.binzen@deutschebahn.de',
+                id: 1,
+            },
+            {
+                vorname: 'Henning',
+                nachname: 'Pagnia',
+                schwerpunkt: 'IT-Sicherheit',
+                phone: '017653725528',
+                mail: 'henning.pagnia@dhbw-mannheim.de',
+                id: 2,
+            },
+            {
+                vorname: 'Michael',
+                nachname: 'Spengler',
+                schwerpunkt: 'Plattformen & Frameworks',
+                phone: '017653725528',
+                mail: 'michael.spengler@sap.com',
+                id: 3,
+            },
+        ],
+    });
+return (
+    <MaterialTable
+        title="Dozenten"
+        columns={state.columns}
+        data={state.data}
+        editable={{
+            onRowAdd: newData =>
+            new Promise(resolve => {
+                setTimeout(() => {
+                resolve();
+                setState(prevState => {
+                    const data = [...prevState.data];
+                    data.push(newData);
+                    return { ...prevState, data };
+                });
+                }, 600);
+            }),
+            onRowUpdate: (newData, oldData) =>
+            new Promise(resolve => {
+                setTimeout(() => {
+                resolve();
+                if (oldData) {
+                    setState(prevState => {
+                    const data = [...prevState.data];
+                    data[data.indexOf(oldData)] = newData;
+                    return { ...prevState, data };
+                    });
+                }
+                }, 600);
+            }),
+            onRowDelete: oldData =>
+            new Promise(resolve => {
+                setTimeout(() => {
+                resolve();
+                setState(prevState => {
+                    const data = [...prevState.data];
+                    data.splice(data.indexOf(oldData), 1);
+                    return { ...prevState, data };
+                });
+                }, 600);
+            }),
+        }}
+        />
+    )
+}
