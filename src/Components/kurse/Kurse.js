59e5096947ef471d92f2a27edc78900016259117
David Letkemann <dsignupl@icloud.com>
2019-12-12 20:04:41 +0100

    Tabs in der 'Kurse'-Ansicht

--- src/Components/kurse/Kurse.js
+++ src/Components/kurse/Kurse.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class Kurse extends Component {
     render() {
         return (
-            <div>
-                <h1>Kurse</h1>
-            </div>
+            <Tabs>
+                <TabList>
+                    <Tab>Übersicht</Tab>
+                    <Tab>WWI 17 SEB</Tab>
+                    <Tab>WWI 18 SEA</Tab>
+                    <Tab>Kurs hinzufügen</Tab>
+                </TabList>
+                <TabPanel>
+                    <h2>Google Calendar Einbindung mit allen Kursen</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Google Calendar nur mit Terminen von WWI 17 SEB</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Google Calendar nur mit Terminen von WWI 18 SEA</h2>
+                </TabPanel>
+            </Tabs>
         )
     }
 }
