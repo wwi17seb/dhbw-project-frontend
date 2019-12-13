e5f00516623d6f64bd6e6b4fa65f5f6ac2c4df94
David Letkemann <dsignupl@icloud.com>
2019-12-13 15:34:22 +0100

    Different Table implementation

--- src/Components/dozenten/Dozenten.js
+++ src/Components/dozenten/Dozenten.js
@@ -1,4 +1,4 @@
-import React, { Component, forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
 import Check from '@material-ui/icons/Check';
@@ -14,12 +14,13 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import './dozenten.css';
 import MaterialTable from 'material-table';
 
 
 export default function DozentenTable() {
-    const tableIcons ={
+    const tableIcons = {
         Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
         Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
         Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
@@ -36,7 +37,8 @@ export default function DozentenTable() {
         Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
         SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
         ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+        Mail: forwardRef((props, ref) => <MailOutlineIcon {...props} ref={ref} />)
     }
     const [state, setState] = React.useState({
         columns: [
@@ -51,7 +53,7 @@ export default function DozentenTable() {
             },
         ],
         data: [
-            { 
+            {
                 vorname: 'Michael',
                 nachname: 'Binzen',
                 schwerpunkt: 'Software-Architektur',
@@ -77,49 +79,56 @@ export default function DozentenTable() {
             },
         ],
     });
-return (
-    <MaterialTable
-        icons={tableIcons}
-        title="Dozenten"
-        columns={state.columns}
-        data={state.data}
-        editable={{
-            onRowAdd: newData =>
-            new Promise(resolve => {
-                setTimeout(() => {
-                resolve();
-                setState(prevState => {
-                    const data = [...prevState.data];
-                    data.push(newData);
-                    return { ...prevState, data };
-                });
-                }, 600);
-            }),
-            onRowUpdate: (newData, oldData) =>
-            new Promise(resolve => {
-                setTimeout(() => {
-                resolve();
-                if (oldData) {
-                    setState(prevState => {
-                    const data = [...prevState.data];
-                    data[data.indexOf(oldData)] = newData;
-                    return { ...prevState, data };
-                    });
+    return (
+        <MaterialTable
+            actions={[
+                {
+                    icon: tableIcons.Mail,
+                    tooltip: 'Send Mail',
+                    onClick: (event, rowData) => window.location.href = "mailto:" + rowData.mail
                 }
-                }, 600);
-            }),
-            onRowDelete: oldData =>
-            new Promise(resolve => {
-                setTimeout(() => {
-                resolve();
-                setState(prevState => {
-                    const data = [...prevState.data];
-                    data.splice(data.indexOf(oldData), 1);
-                    return { ...prevState, data };
-                });
-                }, 600);
-            }),
-        }}
+            ]}
+            icons={tableIcons}
+            title="Dozenten"
+            columns={state.columns}
+            data={state.data}
+            editable={{
+                onRowAdd: newData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            setState(prevState => {
+                                const data = [...prevState.data];
+                                data.push(newData);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            if (oldData) {
+                                setState(prevState => {
+                                    const data = [...prevState.data];
+                                    data[data.indexOf(oldData)] = newData;
+                                    return { ...prevState, data };
+                                });
+                            }
+                        }, 600);
+                    }),
+                onRowDelete: oldData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            setState(prevState => {
+                                const data = [...prevState.data];
+                                data.splice(data.indexOf(oldData), 1);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+            }}
         />
     )
 }
--- src/Components/kurse/Kurse.js
+++ src/Components/kurse/Kurse.js
@@ -1,32 +1,86 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import './kurse.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-class Kurse extends Component {
-    render() {
-        return (
-            <Tabs>
-                <TabList className="tabList">
-                    <Tab>Übersicht</Tab>
-                    <Tab>WWI 17 SEB</Tab>
-                    <Tab>WWI 18 SEA</Tab>
-                    <Tab>Kurs hinzufügen</Tab>
-                </TabList>
-                <TabPanel>
-                    <h2>Google Calendar Einbindung mit allen Kursen</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Google Calendar nur mit Terminen von WWI 17 SEB</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Google Calendar nur mit Terminen von WWI 18 SEA</h2>
-                </TabPanel>
-                <TabPanel>
-                </TabPanel>
-            </Tabs>
-        )
-    }
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
 }
 
-export default Kurse
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ScrollableTabsButtonAuto() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const tabLabels = ["Übersicht", "WWI 17 SEB", "WWI 18 SEA", "Hinzufügen"];
+  const finalTabLabels = [];
+  const finalTabPanels = [];
+  const finalPanelContent = ["Übersicht", "WWI 17 SEB", "WWI 18 SEA", "Hinzufügen"];
+  let tabIndex = 0;
+
+  for (let tabLabel of tabLabels) {
+    finalTabLabels.push(<Tab label={tabLabel} {...a11yProps({tabIndex})} />);
+    finalTabPanels.push(<TabPanel value={value} index={tabIndex}> {finalPanelContent[tabIndex]} </TabPanel>)
+    tabIndex++;
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          {finalTabLabels}
+        </Tabs>
+      </AppBar>
+      {finalTabPanels}
+    </div>
+  );
+}
