4bc6440ccd389b5674191028708c10d6b62b8f80
ErikJanskii <erik.jansky@gmail.com>
2019-12-15 15:33:47 +0100

    Kleiner Änderungen am Login Screen
    Kleiner Änderungen am Login Screen
    - CSS Klassen für Styling
    - Card Alignment provisorisch
    - Background richtig angezeigt

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -9,19 +9,19 @@ class Login extends Component {
         return (
             <div className='background' style={{ backgroundImage: `url(${Background})` }}>
                 <div className='container'>
-                    <div className="card bg-dark border-danger text-light">
+                    <div className="card bg-card-background border-danger text-light">
                         <div className='card-header'>
-                            <h2 className='text-center'>ExoPlan Login</h2>
+                            <h2 class="card-header" className='text-center'>ExoPlan Login</h2>
                         </div>
                         <img className='logo mx-auto' src={Logo} alt='DHBW Logo'></img>
                         <div className="card-body">
                             <form>
                                 <div className="form-group">
-                                    <label forhtml="exampleInputEmail1">E-Mail:</label>
+                                    <label class="card-label" forhtml="exampleInputEmail1">E-Mail:</label>
                                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                                 </div>
                                 <div className="form-group">
-                                    <label forhtml="exampleInputPassword1">Passwort:</label>
+                                    <label class="card-label" forhtml="exampleInputPassword1">Passwort:</label>
                                     <input type="password" className="form-control" id="exampleInputPassword1" />
                                     <Link to="/reset">Passwort vergessen?</Link>
                                 </div>
