e7185ce70c2c22a3ed9502a6262531d719d4a36e
JonasDutzi <jonas@astrid-dutzi.de>
2020-05-15 10:17:53 +0200

    B2 finished login and signup working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Nav from './Components/nav/Nav';
 
-function App() {
-  return (
-    <div className='App'>
-      <Nav></Nav>
-      <h1>Home</h1>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    open: true,
+    message: ''
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Nav>
+        </Nav>
+        <h1>Home</h1>
+        {this.props.location.state !== undefined ?
+          <div className="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{this.props.location.state.message}</strong>
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div> : null}
+        {this.state.message ? <div>{this.state.message}</div> : null}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
