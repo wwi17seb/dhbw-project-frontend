261a3fd7751e08a3fe0f6af6ac03339f46d8dbd1
AndreasGueluekoglu <46897639+AndreasGueluekoglu@users.noreply.github.com>
2020-05-27 13:10:02 +0200

    Outsourcing of LecturerList as .js-data

--- src/Components/dozenten/Dozenten.js
+++ src/Components/dozenten/Dozenten.js
@@ -26,7 +26,8 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import './dozenten.css';
 import MaterialTable from 'material-table';
 import Nav from '../nav/Nav';
-import axios from 'axios';
+import LecturerList from './lecturerlist'
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -172,38 +173,3 @@ export default function DozentenTable() {
         </div>
     )
 }
-/* 
-
-export default class LecturerList extends React.Component {
-  state = {
-    lecturers: []
-  }
-
-  componentDidMount() {
-    axios.get(`api/lecturers`)
-      .then(res => {
-        const lecturers = res.data;
-        this.setState({ lecturers });
-      })
-  }
-
-  render() {
-    return (
-      <ul>
-        { this.state.lecturers.map(lecturers =>
-            <li>lecturers.lecturer_id</li>
-            <li>lecturers.firstname</li>
-            <li>lecturers.lastname</li>
-            <li>lecturers.academic_title</li>
-            <li>lecturers.email</li>
-            <li>lecturers.salutation</li>
-            <li>lecturers.phonenumber</li>
-            <li>lecturers.experience</li>
-            <li>lecturers.comment</li>
-            <li>lecturers.is_extern</li>
-            )}
-      </ul>
-    )
-  }
-}
-*/
