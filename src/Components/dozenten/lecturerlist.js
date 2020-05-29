6816a600680548884e088c5a9725d45ed68e8577
Rolle <julian.rolle@sap.com>
2020-05-29 13:34:05 +0200

    grid layout for lecturerlist, mocked api call
    Co-authored-by: Hawkman59 <falk.ba@gmx.de>

--- src/Components/dozenten/lecturerlist.js
+++ src/Components/dozenten/lecturerlist.js
@@ -1,38 +1,97 @@
 import axios from 'axios';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { Grid } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import * as testdata from "./dozententestdata.json"
 
+const useStyles = makeStyles(theme => ({
+  root: {
+      display: 'flex',
+  }, toolbar: theme.mixins.toolbar,
+  paper: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+  }
+}));
+export default function LecturerList() {
+  const classes = useStyles();
+  const [lecturers,setLecturers] = React.useState(null)
+  const [output,setOutput] = React.useState([])
 
-export default class LecturerList extends React.Component {
-  state = {
-    lecturers: []
+  const loadData = ()=>{
+    axios.get(`api/lecturers`).then(res => {
+      setLecturers(res.data);
+    })
   }
 
-  componentDidMount() {
-    axios.get(`api/lecturers`)
-      .then(res => {
-        const lecturers = res.data;
-        this.setState({ lecturers });
-      })
+  useEffect(()=>{
+    createLecturerRow()
+  },[lecturers])
+
+  const createLecturerRow = ()=>{
+    
+    if(lecturers!==null){
+      var temp=[]
+      for(var i=0; i<lecturers["default"]["payload"]["lecturers"].length;i++){
+        var title= lecturers["default"]["payload"]["lecturers"][i]["academic_title"]
+        var name = lecturers["default"]["payload"]["lecturers"][i]["firstname"]+ " " + lecturers["default"]["payload"]["lecturers"][i]["lastname"]
+        var mainFocus=lecturers["default"]["payload"]["lecturers"][i]["main_focus"]
+        var email=lecturers["default"]["payload"]["lecturers"][i]["email"]
+        temp.push(
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+            <Grid item xs={4}>
+                <Typography variant="h6">{title+" " +name}</Typography>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography variant="h6">{mainFocus}</Typography>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography variant="h6">{email}</Typography>
+            </Grid>
+          </Grid>
+          <Divider></Divider>
+          </Grid>        
+        )
+      }
+      setOutput(temp)
+    }
+    
+    console.log(temp)
+    
+  }
+  
+  if(lecturers === null){
+    //loadData()
+    setLecturers(testdata)
+  }
+    
+  console.log(lecturers)
+
+  return (
+        <Grid container spacing={2}>
+        <Paper className={classes.paper}>
+        <Grid item xs={12} style={{marginBottom:10}}>
+            <Grid container spacing={2}>
+            <Grid item xs={4}>
+                <Typography variant="h5">Name</Typography>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography variant="h5">Schwerpunkt</Typography>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography variant="h5">Email</Typography>
+            </Grid>
+          </Grid>
+          <Divider></Divider>
+          </Grid>    
+          {output}
+        </Paper>
+        
+        </Grid>
+    );
   }
 
-  render() {
-    return (
-      <ul>
-        { this.state.lecturers.map(lecturers =>
-            
-            <><li>lecturers.lecturer_id</li>
-            <li>lecturers.firstname</li>
-            <li>lecturers.lastname</li>
-            <li>lecturers.academic_title</li>
-            <li>lecturers.email</li>
-            <li>lecturers.salutation</li>
-            <li>lecturers.phonenumber</li>
-            <li>lecturers.experience</li>
-            <li>lecturers.comment</li>
-            <li>lecturers.is_extern</li>
-            </>
-            )}
-      </ul>
-    )
-  }
-}
