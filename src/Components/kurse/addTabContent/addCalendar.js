41eaf048c8f38725f36aedc85a09115de092c9f5
ErikJanskii <erik.jansky@gmail.com>
2020-06-12 12:39:34 +0200

    write to google calendar
    write to google calendar with example events and still on the wrong calendar (my own and not the ProjectWWi17SEB)

--- src/Components/kurse/addTabContent/addCalendar.js
+++ src/Components/kurse/addTabContent/addCalendar.js
@@ -61,7 +61,44 @@ const handleClick = () => {
 
       gapi.auth2.getAuthInstance().signIn().then(
           () => {
-              console.log("signed in")
+
+            var event = {
+              'summary': 'Google I/O 2015',
+              'location': '800 Howard St., San Francisco, CA 94103',
+              'description': 'A chance to hear more about Google\'s developer products.',
+              'start': {
+                'dateTime': '2020-05-28T09:00:00-07:00',
+                'timeZone': 'America/Los_Angeles'
+              },
+              'end': {
+                'dateTime': '2020-05-28T17:00:00-08:00',
+                'timeZone': 'America/Los_Angeles'
+              },
+              'recurrence': [
+                'RRULE:FREQ=DAILY;COUNT=2'
+              ],
+              'attendees': [
+                {'email': 'lpage@example.com'},
+                {'email': 'sbrin@example.com'}
+              ],
+              'reminders': {
+                'useDefault': false,
+                'overrides': [
+                  {'method': 'email', 'minutes': 24 * 60},
+                  {'method': 'popup', 'minutes': 10}
+                ]
+              }
+            };
+
+            var request = gapi.client.calendar.events.insert({
+              'calendarId': 'primary',
+              'resource': event
+            });
+            
+            request.execute(function(event) {
+              window.open(event.htmlLink);
+            });
+
           }
       )
   })
