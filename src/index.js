842dccd34e3b10d4bcd65a9341c01b9d698e5b51
D070704 <verena.richarz@sap.com>
2020-06-28 20:46:34 +0200

    createMuiTheme

--- src/index.js
+++ src/index.js
@@ -11,6 +11,23 @@ import ModulkatalogDetail from './Components/modulkatalog/ModulkatalogDetail'
 import Login from './Components/login/Login'
 import ResetPassword from './Components/login/forgotPassword'
 import Kontoeinstellungen from './Components/kontoeinstellungen/Kontoeinstellugen'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+// Color theme for whole app
+const theme = createMuiTheme({
+  palette: {
+     primary: {
+        main: '#5c6971',
+        light: '#89979f',
+        dark: '#323e46'
+     },
+     secondary: {
+       main: '#F96523',
+       light: '#ff5444',
+       dark: '#a70000'
+     },
+  }
+});
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={(props) => (
@@ -21,16 +38,18 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 )
 
 const routing = (
-  <Router>
-    <div>
-      <Route path="/kontoeinstellungen" component={Kontoeinstellungen} />
-      <Route path="/kurse" component={Kurse} />
-      <Route path="/dozenten" component={Dozenten} />
-      <Route exact path='/modulkatalog' component={Modulkatalog} />
-      <Route exact path='/modulkatalog/details/:name' component={ModulkatalogDetail} />
-      <Route exact path='/' component={Login} />
-      <Route exact path="/reset" component={ResetPassword} />
-    </div>
-  </Router>
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <div>
+        <Route path="/kontoeinstellungen" component={Kontoeinstellungen} />
+        <Route path="/kurse" component={Kurse} />
+        <Route path="/dozenten" component={Dozenten} />
+        <Route exact path='/modulkatalog' component={Modulkatalog} />
+        <Route exact path='/modulkatalog/details/:name' component={ModulkatalogDetail} />
+        <Route exact path='/' component={Login} />
+        <Route exact path="/reset" component={ResetPassword} />
+      </div>
+    </Router>
+  </MuiThemeProvider>
 )
 ReactDOM.render(routing, document.getElementById('root'))
