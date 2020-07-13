e68b6b28414d2519c25af764cd740bdc444a78ff
Hawk <falk.ba@gmx.de>
2020-07-13 17:33:15 +0200

    removed unnecessary imports

--- src/Components/kurse/editkurs/editkurs.js
+++ src/Components/kurse/editkurs/editkurs.js
@@ -1,20 +1,16 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import StudiengangAuswahl from '../addkurs/studiengangauswahl'
 import { APICall } from '../../../helper/Api';
-import SemesterAuswahl from '../addkurs/semesterauswahl'
 import SubmitFeedback from '../addkurs/submitfeedback'
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Switch from '@material-ui/core/Switch';
 import { Typography } from '@material-ui/core';
-import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles(theme => ({
     textfield: {
